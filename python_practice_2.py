f900d571e319f09809e77896c082e95b5d994ea2
gitminjung <minjung1720@naver.com>
2024-03-24 00:19:34 +0900

    Add TextFile[python_practice_2.py]

---
+++ python_practice_2.py
@@ -0,0 +1,55 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=== RESTART: C:/Users/yeonh/AppData/Local/Programs/Python/Python312/주석 달기.py ===
+A
+B
+
+=== RESTART: C:/Users/yeonh/AppData/Local/Programs/Python/Python312/주석 달기.py ===
+10
+5
+
+=== RESTART: C:/Users/yeonh/AppData/Local/Programs/Python/Python312/주석 달기.py ===
+10
+5
+17
+
+=== RESTART: C:/Users/yeonh/AppData/Local/Programs/Python/Python312/주석 달기.py ===
+hi hello
+
+=== RESTART: C:/Users/yeonh/AppData/Local/Programs/Python/Python312/주석 달기.py ===
+이름을 입력해주세요.
+=== RESTART: C:/Users/yeonh/AppData/Local/Programs/Python/Python312/주석 달기.py ===
+이름을 입력해주세요.lululu
+name
+'lululu'
+>>> 
+=== RESTART: C:/Users/yeonh/AppData/Local/Programs/Python/Python312/주석 달기.py ===
+이름을 입력해주세요.hate
+>>> study
+'hate'
+>>> 
+=== RESTART: C:/Users/yeonh/AppData/Local/Programs/Python/Python312/주석 달기.py ===
+이름을 입력해주세요.
+=== RESTART: C:/Users/yeonh/AppData/Local/Programs/Python/Python312/주석 달기.py ===
+이름을 입력해주세요.love
+이름을 입력해주세요 love
+>>> 
+=== RESTART: C:/Users/yeonh/AppData/Local/Programs/Python/Python312/주석 달기.py ===
+이름을 입력해주세요.hi
+안녕하세요 hi
+>>> 
+=== RESTART: C:/Users/yeonh/AppData/Local/Programs/Python/Python312/주석 달기.py ===
+내 이름은.minjung
+Traceback (most recent call last):
+  File "C:/Users/yeonh/AppData/Local/Programs/Python/Python312/주석 달기.py", line 2, in <module>
+    print('박',study)
+NameError: name 'study' is not defined
+>>> 
+=== RESTART: C:/Users/yeonh/AppData/Local/Programs/Python/Python312/주석 달기.py ===
+내 이름은.minjung
+박 minjung
+>>> 
+=== RESTART: C:/Users/yeonh/AppData/Local/Programs/Python/Python312/주석 달기.py ===
+코딩.practice
+어려워 practice
