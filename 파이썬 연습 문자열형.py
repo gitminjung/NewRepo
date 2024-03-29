a054d1e336c47cf39e28b29736d8d3f8c2cf85cd
gitminjung <162651657+gitminjung@users.noreply.github.com>
2024-03-29 12:54:53 +0900

    Add files via upload

---
+++ 파이썬 연습 문자열형.py
@@ -0,0 +1,7 @@
+a=input('숫자 하나 입력')
+b=int(a)
+c=float(a)
+
+print(type(a))
+print(type(b))
+print(type(c))
---
+++ 파이썬 연습 자료형.py
@@ -0,0 +1,13 @@
+a=10
+b=5
+c=2.0
+d=0.5
+
+
+print(b+c)
+print(b-c)
+print(b*c)
+print(b/c)
+print(b//c)
+print(b%c)
+print(b**c)
