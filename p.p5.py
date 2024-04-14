c515fb1b9bc422d8a69fa9d0247fab407979d948
gitminjung <162651657+gitminjung@users.noreply.github.com>
2024-04-15 01:55:48 +0900

    Add files via upload
    파이썬 숫자 다루기 연습

---
+++ p.p5.py
@@ -0,0 +1,60 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========================================================================================== RESTART: C:/Users/82108/AppData/Local/Programs/Python/Python312/python_practice4.py ==========================================================================================
+a 입력:3
+b 입력:6
+c 입력:9
+3 6 9 18
+
+=========================================================================================== RESTART: C:/Users/82108/AppData/Local/Programs/Python/Python312/python_practice4.py ==========================================================================================
+a 입력:3
+b 입력:6
+c 입력:9
+3 6 9 369
+
+= RESTART: C:/Users/82108/AppData/Local/Programs/Python/Python312/python_practice4.py
+a 입력:
+= RESTART: C:/Users/82108/AppData/Local/Programs/Python/Python312/python_practice4.py
+Traceback (most recent call last):
+  File "C:/Users/82108/AppData/Local/Programs/Python/Python312/python_practice4.py", line 3, in <module>
+    print(a,b,c,a+b+c)
+NameError: name 'a' is not defined
+
+= RESTART: C:/Users/82108/AppData/Local/Programs/Python/Python312/python_practice4.py
+a b c값 입력3 6 9
+3 6 9 18
+>>> 
+= RESTART: C:/Users/82108/AppData/Local/Programs/Python/Python312/python_practice4.py
+날짜 입력 yyyy.mm.dd2024.04.14
+2024.04.14 2024 04 14
+>>> type(y)
+<class 'str'>
+>>> typr(m)
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    typr(m)
+NameError: name 'typr' is not defined. Did you mean: 'type'?
+>>> type(m)
+<class 'str'>
+>>> type(d)
+<class 'str'>
+>>> y
+'2024'
+>>> m
+'04'
+>>> d
+'14'
+>>> 
+= RESTART: C:/Users/82108/AppData/Local/Programs/Python/Python312/python_practice4.py
+Traceback (most recent call last):
+  File "C:/Users/82108/AppData/Local/Programs/Python/Python312/python_practice4.py", line 1, in <module>
+    a,b,c=map(int['1','2','3'])
+TypeError: type 'int' is not subscriptable
+>>> 
+= RESTART: C:/Users/82108/AppData/Local/Programs/Python/Python312/python_practice4.py
+1 2 3 6
+>>> 
+= RESTART: C:/Users/82108/AppData/Local/Programs/Python/Python312/python_practice4.py
+a b c값 입력3 6 9
+3 6 9 18
